d6f530ecfbe253c3d08706fe4e90465afedbc18c
Diego Enriquez-Serrano <diego@diegoenriquezserrano.dev>
2026-01-06 15:20:48 -0600

    Adds GeoJsonParse struct to handle fetching and parsing data

--- src/json_parser.rs
+++ src/json_parser.rs
@@ -1,83 +1,117 @@
 use anyhow::{Context, Result};
 use geojson::{Feature, FeatureCollection, GeoJson, Geometry, JsonObject, JsonValue};
 use serde::{Deserialize, Serialize};
-use serde_json::{from_str, to_string};
+use serde_json::Value;
+use std::path::Path;
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct InputJson<'a> {
-    #[serde(rename = "geocoded_column")]
-    geometry: Geometry,
-    crash_circumstances: Option<String>,
-    crash_date: &'a str,
-    crash_location: &'a str,
-    latitude: &'a str,
-    longitude: &'a str,
-    victim: &'a str,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GeoJsonParser {
+    pub geojson: GeoJson,
 }
 
-pub fn parse_file(filename: String) -> Result<String, anyhow::Error> {
-    let json = std::fs::read_to_string(&filename)
-        .with_context(|| format!("could not read file `{}`", filename))?;
-    let input_vec = from_str::<Vec<InputJson>>(&json)
-        .with_context(|| format!("could not json from `{}`", json))?;
-    let feature_vec = transform_to_geojson_feature_vec(input_vec);
-    let geojson = make_feature_collection(feature_vec);
-
-    to_string(&geojson).with_context(|| format!("Failed to parse JSON from: {}", geojson))
-}
-
-fn transform_to_geojson_feature_vec(input_vec: Vec<InputJson>) -> Vec<Feature> {
-    let mut feature_vec: Vec<Feature> = Vec::new();
-
-    for input_entry in &input_vec {
-        let geojson = GeoJson::Feature(Feature {
-            bbox: None,
-            geometry: Some(input_entry.geometry.clone()),
-            id: None,
-            properties: Some(geo_properties_from_input_json(input_entry)),
-            foreign_members: None,
-        });
-
-        let feature = Feature::try_from(geojson).expect("Failed to parse Feature from GeoJSON");
-
-        feature_vec.push(feature);
+impl GeoJsonParser {
+    pub fn parse_file<P>(path: P) -> Result<Self, anyhow::Error>
+    where
+        P: AsRef<Path> + std::fmt::Debug,
+    {
+        let json = Self::read_from_file(path)?;
+        Self::parse_raw(json)
+    }
+    pub fn pull_and_parse() -> Result<Self, anyhow::Error> {
+        let raw_data =
+            reqwest::blocking::get("https://data.cityofchicago.org/resource/gzaz-isa6.json")?
+                .text()
+                .context("Failed to get vision zero data.")?;
+        Self::parse_raw(raw_data)
     }
 
-    feature_vec
+    pub fn parse_raw(text: String) -> Result<Self, anyhow::Error> {
+        let inputs = Self::get_input_list(text)?;
+        let features = Self::convert_to_feature_list(inputs)?;
+
+        Ok(Self::make_feature_collection(features))
+    }
+
+    fn make_feature_collection(input_features: Vec<Feature>) -> Self {
+        let feature_collection = FeatureCollection {
+            bbox: None,
+            features: input_features,
+            foreign_members: None,
+        };
+
+        Self {
+            geojson: GeoJson::from(feature_collection),
+        }
+    }
+
+    fn convert_to_feature_list(
+        entries: Vec<VisionZeroData>,
+    ) -> Result<Vec<Feature>, anyhow::Error> {
+        let mut feature_vec: Vec<Feature> = Vec::new();
+
+        for input_entry in entries {
+            let feature: Feature = input_entry.try_into()?;
+            feature_vec.push(feature);
+        }
+
+        Ok(feature_vec)
+    }
+
+    fn get_input_list(json: String) -> Result<Vec<VisionZeroData>, anyhow::Error> {
+        serde_json::from_str(&json).with_context(|| format!("could not parse json from `{}`", json))
+    }
+
+    fn read_from_file<P>(path: P) -> Result<String, anyhow::Error>
+    where
+        P: AsRef<Path> + std::fmt::Debug,
+    {
+        std::fs::read_to_string(&path).with_context(|| format!("could not read file `{:?}`", path))
+    }
 }
 
-fn make_feature_collection(input_features: Vec<Feature>) -> GeoJson {
-    let feature_collection = FeatureCollection {
-        bbox: None,
-        features: input_features,
-        foreign_members: None,
-    };
-
-    GeoJson::from(feature_collection)
+#[derive(Debug, Deserialize, Serialize)]
+struct VisionZeroData {
+    crash_circumstances: Option<String>,
+    crash_date: String,
+    crash_location: String,
+    #[serde(rename = "geocoded_column")]
+    geometry: Geometry,
+    latitude: String,
+    longitude: String,
+    victim: String,
 }
 
-fn geo_properties_from_input_json(input_json: &InputJson) -> JsonObject {
-    let mut properties = JsonObject::new();
+impl Into<JsonObject> for VisionZeroData {
+    fn into(self) -> JsonObject {
+        let mut properties = JsonObject::new();
+        properties.insert(
+            "crash_circumstances".to_string(),
+            JsonValue::from(self.crash_circumstances.as_deref()),
+        );
+        properties.insert("crash_date".to_string(), Value::from(self.crash_date));
+        properties.insert(
+            "crash_location".to_string(),
+            Value::from(self.crash_location),
+        );
+        properties.insert("latitude".to_string(), Value::from(self.latitude));
+        properties.insert("longitude".to_string(), Value::from(self.longitude));
+        properties.insert("victim".to_string(), Value::from(self.victim));
+        properties
+    }
+}
 
-    properties.insert(
-        "crash_circumstances".to_string(),
-        JsonValue::from(input_json.crash_circumstances.as_deref()),
-    );
-    properties.insert(
-        "crash_date".to_string(),
-        JsonValue::from(input_json.crash_date),
-    );
-    properties.insert(
-        "crash_location".to_string(),
-        JsonValue::from(input_json.crash_location),
-    );
-    properties.insert("latitude".to_string(), JsonValue::from(input_json.latitude));
+impl TryInto<Feature> for VisionZeroData {
+    type Error = anyhow::Error;
 
-    properties.insert(
-        "longitude".to_string(),
-        JsonValue::from(input_json.longitude),
-    );
-    properties.insert("victim".to_string(), JsonValue::from(input_json.victim));
+    fn try_into(self) -> Result<Feature, Self::Error> {
+        let geojson = GeoJson::Feature(Feature {
+            bbox: None,
+            foreign_members: None,
+            geometry: Some(self.geometry.clone()),
+            id: None,
+            properties: Some(self.into()),
+        });
 
-    properties
+        Feature::try_from(geojson).context("Failed to parse Feature from GeoJSON.")
+    }
 }
